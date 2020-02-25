c4aa775529300ebe638fa81f73fc726c4a67a9c5
Garrett James <garrettjames55@gmail.com>
2020-02-25 13:30:51 -0500

    updated portfolio

---
+++ main.js
@@ -0,0 +1,23 @@
+//nav-bar Scroll Feature
+$('a[href^=\'#\']').click(function(e) {
+  e.preventDefault();
+	
+  var position = $($(this).attr('href')).offset().top;
+
+  $('body, html').animate({
+    scrollTop: position
+  });
+});
+
+//About Me and Project Scroll Feature
+$('.aboutme-section').click(function (){
+  $('html, body').animate({
+    scrollTop: $('#section1').offset().top
+  }, 1600);
+});
+
+$('.project-section').click(function (){
+  $('html, body').animate({
+    scrollTop: $('#section2').offset().top
+  }, 1400);
+});
